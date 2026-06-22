c3e2e049031d916d628adeec135dc00f741ad62a
Jalpachavda-hb <jalpachavdahackbarry@gmail.com>
2026-06-22 13:05:36 +0530

    refactor: remove basic-ssl plugin and clean up commented code in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,45 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { fileURLToPath } from 'url'
-// import path from 'path'
-
-// const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-// export default defineConfig({
-//   plugins: [react()],
-  
-//   server: {
-//     host: '0.0.0.0',
-//     port: 5173,
-//   },
-
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl' // 1. Import basic-ssl
 import { fileURLToPath } from 'url'
 import path from 'path'
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  plugins: [
-    react(), 
-    basicSsl() // 2. Add to plugins array
-  ],
+  plugins: [react()],
   
   server: {
     host: '0.0.0.0',
     port: 5173,
-
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import basicSsl from '@vitejs/plugin-basic-ssl' // 1. Import basic-ssl
+// import { fileURLToPath } from 'url'
+// import path from 'path'
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
+// export default defineConfig({
+//   plugins: [
+//     react(), 
+//     basicSsl() // 2. Add to plugins array
+//   ],
+  
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
